d8fb23fbf709e746ff35385726f93ae204398f9d
dheeraj1429 <dheerajsingh1429@gmail.com>
2023-04-29 15:19:32 +0530

    apollo with mongodb aggregation

--- server.js
+++ server.js
@@ -3,38 +3,37 @@ const express = require('express');
 const path = require('path');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 const { loadFilesSync } = require('@graphql-tools/load-files');
-const { graphqlHTTP } = require('express-graphql');
 const databaseConnection = require('./model/database/db');
+const { ApolloServer } = require('apollo-server-express');
+const port = process.env.PORT || 3000;
 
 const schemaArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
 const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
 
-const schema = makeExecutableSchema({
-   typeDefs: schemaArray,
-   resolvers: resolversArray,
-});
+async function startApolloServer() {
+   const app = express();
 
-const port = process.env.PORT || 3000;
-const app = express();
-
-const customlMiddleware = function (req, res, next) {
-   console.log('from(-- middleware');
-   next();
-};
-
-app.use('/graphql', customlMiddleware, (req, res) => {
-   graphqlHTTP({
-      schema: schema,
-      graphiql: true,
-   })(req, res);
-});
-
-app.get('/', (req, res, next) => {
-   res.send(`<h1>Let's start our story with graphql...</h1>`);
-});
-
-databaseConnection(() => {
-   app.listen(port, () => {
-      console.log(`server running on port ${port}...`);
+   const schema = makeExecutableSchema({
+      typeDefs: schemaArray,
+      resolvers: resolversArray,
    });
-});
+
+   const server = new ApolloServer({
+      schema,
+   });
+
+   await server.start();
+   server.applyMiddleware({ app, path: '/graphql' });
+
+   app.get('/', (req, res, next) => {
+      res.send(`<h1>Let's start our story with graphql...</h1>`);
+   });
+
+   databaseConnection(() => {
+      app.listen(port, () => {
+         console.log(`server running on port ${port}...`);
+      });
+   });
+}
+
+startApolloServer();
